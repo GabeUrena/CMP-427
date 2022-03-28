c5adcb4449a663bf9cbe32632857a4e1c83a7464
Gabriel Urena <54905586+GabeUrena@users.noreply.github.com>
2022-03-28 16:28:43 -0400

    Update SudokuValidator.java

--- SudokuValidator.java
+++ SudokuValidator.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class SudokuValidator implements Runnable{
 	// Variable for the puzzle board 9x9
 	private int[][] sudokuPuzzle = new int[9][9] ;
@@ -6,6 +8,7 @@ public class SudokuValidator implements Runnable{
 	public SudokuValidator() {
 		
 	}
+	
 	// Constructor with puzzle parameter
 	public SudokuValidator(int[][] puzzle) {
 	// Copying given 2D array to the private 2d array
@@ -23,26 +26,26 @@ public class SudokuValidator implements Runnable{
 	
 	// Checking if the 3x3 grids are valid
 	public boolean gridCheck() {
-	//9 grids
-	// iterate through 9 times
-		for(int i = 0; i<9;i++) {
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-			
-		}
-			
-		
-		return false;
+		boolean[] tempArr = new boolean[10];
+	// Switching through each grid segment
+			for(int segRow = 0; segRow < 9; segRow += 3) {
+				for(int segCol = 0; segCol < 9; segCol += 3) {
+					Arrays.fill(tempArr, false);
+	// Checking each number of each grid
+					for(int gridRow = 0; gridRow < 3; gridRow++) {
+						for(int gridCol = 0; gridCol < 3; gridCol++) {
+	// Grab number from the grid segment within the puzzle
+							int num = this.sudokuPuzzle[segRow+gridRow][segCol+gridCol];
+	// If the selected number shows up again return false 
+							if(tempArr[num]) {
+								return false;
+							}
+							tempArr[num] = true;
+						}	
+					}
+				}	
+			}
+		return true;
 	}
 	
 	// Checking if the 9 rows are valid
@@ -52,13 +55,13 @@ public class SudokuValidator implements Runnable{
 			for(int col = 0; col < 9; col++) {
 	// grab number from board
 				int temp = this.sudokuPuzzle[row][col];
-				int occurence = 0;
+				int occur = 0;
 	// check if number repeats within its row
 				for(int i = 0; i <9; i++) {
 					if(temp == this.sudokuPuzzle[row][i]  ) {
-						occurence++;
+						occur++;
 	// number should only appear once, if its twice we have a duplicate
-							if(occurence > 1) {	
+							if(occur > 1) {	
 								return false;
 							}
 					}
@@ -75,13 +78,13 @@ public class SudokuValidator implements Runnable{
 			for(int row = 0; row < 9; row++) {
 	// grab number from board
 				int temp = this.sudokuPuzzle[row][col];
-				int occurence = 0;
+				int occur = 0;
 	// check if number repeats within its row
-				for(int k = 0; k <9; k++) {
-					if(temp == this.sudokuPuzzle[k][col]  ) {
-						occurence++;
+				for(int i = 0; i <9; i++) {
+					if(temp == this.sudokuPuzzle[i][col]  ) {
+						occur++;
 	// number should only appear once, if its twice we have a duplicate
-							if(occurence > 1) {	
+							if(occur > 1) {	
 								return false;
 							}
 					}
@@ -89,10 +92,6 @@ public class SudokuValidator implements Runnable{
 			}
 		}
 		return true;
-	}
-	
-	public boolean isSudokuValid() {
-		return (gridCheck() && rowCheck() && columnCheck());
 	}
 	
 	// run method for threads
@@ -105,14 +104,12 @@ public class SudokuValidator implements Runnable{
 				System.out.println("Row results: " + this.rowCheck());
 			} else if(threadName.equals("column")) {
 				System.out.println("Column results: " + this.columnCheck());
-			}
-			
+			}	
 	}
 	
 	public static void main(String[] args) {
-		//note: implement a user input system for puzzle
-		// Sudoku board as a 2d array 
-		int[][] puzzleInput = {{6, 2, 4, 5, 3, 9, 1, 8, 7},
+	// Sudoku board as a 2d array 
+	 		int[][] puzzleInput = {{6, 2, 4, 5, 3, 9, 1, 8, 7},
 						      {5, 1, 9, 7, 2, 8, 6, 3, 4},
 						      {8, 3, 7, 6, 1, 4, 2, 9, 5},
 						      {1, 4, 3, 8, 6, 5, 7, 2, 9},
@@ -121,9 +118,9 @@ public class SudokuValidator implements Runnable{
 						      {3, 7, 1, 9, 5, 6, 8, 4, 2},
 						      {4, 9, 6, 1, 8, 2, 5, 7, 3},
 						      {2, 8, 5, 4, 7, 3, 9, 1, 6}};
-	/*	
+	/*
 	//row test case
-	 
+	
 		int[][] puzzleInput = {{1, 2, 3, 4, 5, 6, 7, 8, 9},
 			      {1, 2, 3, 4, 5, 6, 7, 8, 9},
 			      {1, 2, 3, 4, 5, 6, 7, 8, 9},
@@ -145,10 +142,20 @@ public class SudokuValidator implements Runnable{
 			      {3, 3, 3, 3, 3, 3, 3, 3, 3},
 			      {2, 2, 2, 2, 2, 2, 2, 2, 2},
 			      {1, 1, 1, 1, 1, 1, 1, 1, 1}};
-		*/
-		
+			         
+			      
+	   //grid test case
+	 int[][] puzzleInput = {{1, 2, 3, 1, 2, 3, 1, 2, 3},
+						      {4, 5, 6, 4, 5, 6, 4, 5, 6},
+						      {7, 8, 9, 7, 8, 9, 7, 8, 9},
+						      {1, 2, 3, 1, 2, 3, 1, 2, 3},
+						      {4, 5, 6, 4, 5, 6, 4, 5, 6},
+						      {7, 8, 9, 7, 8, 9, 7, 8, 9},
+						      {1, 2, 3, 1, 2, 3, 1, 2, 3},
+						      {4, 5, 6, 4, 5, 6, 4, 5, 6},
+						      {7, 8, 9, 7, 8, 9, 7, 8, 9}};
+			 */
 		SudokuValidator sudokuValidator = new SudokuValidator(puzzleInput);
-		
 		
 		Thread gridThread = new Thread(sudokuValidator,"grid");
 		Thread rowThread = new Thread(sudokuValidator,"row");
@@ -158,16 +165,5 @@ public class SudokuValidator implements Runnable{
 		rowThread.start();
 		columnThread.start();
 		
-		System.out.println("**********************************************"
-						+ "\nIs The Sudoku Solution Valid: "+sudokuValidator.isSudokuValid()
-						+ "\n**********************************************");
-		}
-		
-	
-	
-	
-	
-	
-	
-	
+		}	
 }
